17a233c0d63316bd96f2509a52093de988f725f5
NightSky17 <joyint23@yandex.ru>
2024-12-09 21:34:37 +0300

    HomeWork_Cycles
    Add HomeWork_Cycles (09.12.2024)

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                                                                    ЭТО ЗАДАНИЕ №1
+                    int prev = 1;
+                    int curr = 1;
+                    Console.WriteLine("1\n1");
+                            for (int i = 1; i <= 8; i++)
+                    {
+                        int next = prev + curr;
+                        Console.WriteLine(prev + curr);
+                        prev = curr;
+                        curr = next;
+                    }
+                                                                    ЭТО ЗАДАНИЕ №2
+                    for (int i = 2; i <= 20; i=i+2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                                                                    ЭТО ЗАДАНИЕ №3
+                    int n = 1;
+                    for (int i = 1; i < 10; i++)
+                    {
+                        for (int j = 1; j < 5; j++)
+                        Console.Write(j + "*" + i + "=" + j * i + "    ");
+                        Console.WriteLine();
+                                }
+                                                                    ЭТО ЗАДАНИЕ №4*/
+            string password = "qwerty";
+            string a = Console.ReadLine();
+            while (a != password)
+            {
+                a = Console.ReadLine();
+            }
+            Console.WriteLine("Добро пожаловать!");
+        }
+    }
+}
